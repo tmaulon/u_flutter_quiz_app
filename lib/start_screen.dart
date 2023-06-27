995fe6f0dd3065da3b9b9f5f9bdfd82c89494c33
Thomas Maulon <tmaulon@betomorrow.com>
2023-06-28 00:44:26 +0200

    adding transparency to widgets

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -17,7 +17,16 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
+
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     "assets/images/quiz-logo.png",
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(
             height: 80,
           ),
