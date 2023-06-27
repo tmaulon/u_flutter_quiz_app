ffd12f4187b5febd3a7445cf8fc3fe41dee4bd63
Thomas Maulon <tmaulon@betomorrow.com>
2023-06-28 01:00:25 +0200

    adding stateful  widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:u_flutter_quiz_app/start_screen.dart';
+import 'package:u_flutter_quiz_app/quiz.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,29 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  startQuiz() {
-    print('Starting quiz ...');
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quiz App',
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 173, 240, 223),
-                Color.fromARGB(255, 0, 255, 191)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return const Quiz();
   }
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:u_flutter_quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 173, 240, 223),
+                Color.fromARGB(255, 0, 255, 191)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
