46208f14e12173f43100036395ed3d599585eb74
Thomas Maulon <tmaulon@betomorrow.com>
2023-07-25 15:44:21 +0200

    alignment margin and padding questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,22 +18,28 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(
-            fontSize: 24,
-            color: Color.fromARGB(255, 30, 144, 121),
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        ...currentQuestion.answers
-            .map((answer) => AnswerButton(answerText: answer, onTap: () {})),
-      ]),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                currentQuestion.text,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 30, 144, 121),
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ...currentQuestion.answers.map(
+                  (answer) => AnswerButton(answerText: answer, onTap: () {})),
+            ]),
+      ),
     );
   }
 }
