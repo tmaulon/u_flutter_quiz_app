1a8231b82685fcea1eb2b749da1736651a5e9ac2
Thomas Maulon <tmaulon@betomorrow.com>
2023-07-27 17:23:42 +0200

    Combining columns and rows

--- lib/components/questions_summary.dart
+++ lib/components/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/question_summary.dart';
 
@@ -9,6 +10,11 @@ class QuizSummary extends StatelessWidget {
   });
 
   final List<QuestionSummary> quizSummary;
+
+  bool isValidAnswer(
+      {required String user_awser, required String valid_answer}) {
+    return user_awser == valid_answer;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,15 +27,40 @@ class QuizSummary extends StatelessWidget {
                 Text("${questionSummary.question_index + 1}"),
                 Column(
                   children: [
-                    Text(questionSummary.question),
-                    const SizedBox(
-                      height: 10,
+                    Text(
+                      questionSummary.question,
+                      style: GoogleFonts.lato(
+                        fontSize: 16,
+                        color: const Color.fromARGB(255, 30, 144, 121),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    Text(questionSummary.user_awser),
                     const SizedBox(
-                      height: 10,
+                      height: 5,
                     ),
-                    Text(questionSummary.valid_answer),
+                    Text(
+                      questionSummary.user_awser,
+                      style: GoogleFonts.lato(
+                        fontSize: 12,
+                        color: isValidAnswer(
+                                user_awser: questionSummary.user_awser,
+                                valid_answer: questionSummary.valid_answer)
+                            ? const Color.fromARGB(255, 13, 64, 54)
+                            : const Color.fromARGB(255, 144, 30, 30),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      questionSummary.valid_answer,
+                      style: GoogleFonts.lato(
+                        fontSize: 12,
+                        color: const Color.fromARGB(255, 30, 144, 121),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ],
